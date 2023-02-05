b65afbf88c14d6ade4fa1aedb3b682a953253f38
FunkyGingerCat <funkygingercat@proton.me>
2023-02-05 19:04:35 -0300

    Utilizando laços com For

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -8,14 +8,19 @@ total_de_tentativas = 3
 rodada = 1
 
 #condições do jogo
-while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
     #entrada do chute
-    chute_str = input("Digite o seu número: ")
-
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
-
     chute = int(chute_str)
+
+    #condição de entrada
+    if (chute <1 or chute >100):
+        print ("Você deve digitar um número entre 1 e 100!")
+        continue
+
     #condições de vitória
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -23,12 +28,11 @@ while (rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou")
+        break
     else:
         if(maior):
             print("Você errou, seu chute foi maior do que o número secreto!")
         elif(menor):
-            print("Você errou, seu chute foi menor do que o número secreto!")
-
-    rodada = rodada + 1        
+            print("Você errou, seu chute foi menor do que o número secreto!")    
 
 print ("Fim de jogo")    
