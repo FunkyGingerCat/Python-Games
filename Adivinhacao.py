4cc338ca3712978512465d5456fcac3448e8ae06
FunkyGingerCat <funkygingercat@proton.me>
2023-02-05 17:20:18 -0300

    Utilizando laços com While

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -2,13 +2,33 @@ print ("*********************************")
 print ("Bem vindo ao jogo de Adivinhação!")
 print ("*********************************")
 
+#parâmetros do jogo
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = input("Digite o seu número: ")
+#condições do jogo
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    #entrada do chute
+    chute_str = input("Digite o seu número: ")
 
-print("Você digitou ", chute)
+    print("Você digitou ", chute_str)
 
-if (numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")    
+    chute = int(chute_str)
+    #condições de vitória
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou")
+    else:
+        if(maior):
+            print("Você errou, seu chute foi maior do que o número secreto!")
+        elif(menor):
+            print("Você errou, seu chute foi menor do que o número secreto!")
+
+    rodada = rodada + 1        
+
+print ("Fim de jogo")    
